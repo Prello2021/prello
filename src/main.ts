223948213f83aff20ec13ece9212fbdca6683bfd
kunrenyouAccount <1214kunrenyou@gmail.com>
2021-05-23 16:03:30 +0900

    4000ポートでリクエストOK。nodemodeでホットリロードも有効化。

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
-const test = "typescript修正";
+import express, { Request, Response } from "express";
+import { AddressInfo } from "net";
 
-console.log(test);
+//定義
+const app = express();
+
+//expressで4000ポートにサーバー起動
+const server = app.listen(4000, () => {
+  const address = server.address() as AddressInfo;
+  console.log(`Node.js is listening to PORT: ${address.port}`);
+});
+
+// expressの設定(cors method header 許可の設定)
+app.disable("x-powered-by");
+app.get("/", (req: Request, res: Response) => {
+  const param = { result: "Hello !" };
+  res.header("Content-Type", "application/json; charset=utf-8");
+  res.send(param);
+});
