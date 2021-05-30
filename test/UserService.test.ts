23828d151dca5018c978cdf3d3c678d761b6d520
mattsershaw <buddylfs@gmail.com>
2021-05-30 13:55:29 +0900

    createMock..の関数に引数に渡す機能を実装

--- test/UserService.test.ts
+++ test/UserService.test.ts
@@ -1,39 +1,51 @@
 import { User } from "../src/models/User";
 import { UserService } from "../src/services/UserService";
 import { IUserRepository } from "../src/repositories/interfaces/IUserRepository";
+import { DatabaseResult } from "../src/utils/database/Database";
 
-const mockUserList: User[] = [
-  {
-    id: 1,
-    name: "test1",
-    hobby: "test1",
-  },
-  {
-    id: 2,
-    name: "test2",
-    hobby: "test2",
-  },
-  {
-    id: 3,
-    name: "test3",
-    hobby: "test3",
-  },
-];
-function createMockUserRepository(): IUserRepository {
+const mockUserList: DatabaseResult<User[]> = {
+  value: [
+    {
+      id: 1,
+      name: "test1",
+      hobby: "test1",
+    },
+    {
+      id: 2,
+      name: "test2",
+      hobby: "test2",
+    },
+    {
+      id: 3,
+      name: "test3",
+      hobby: "test3",
+    },
+  ],
+  // error: null,
+};
+
+function createMockGetAllRepository(
+  mockResult: DatabaseResult<User[]>
+): IUserRepository {
   const mockRepository: IUserRepository = {
     getAll: jest.fn(
-      () => new Promise<User[]>((resolve) => resolve(mockUserList))
+      () =>
+        new Promise<DatabaseResult<User[]>>((resolve) => resolve(mockResult))
     ),
   };
 
   return mockRepository;
 }
+// ↑下準備
+// ↓テスト内容の実行
 
 describe("UserService 正常系テスト", () => {
   it("getAll", async () => {
-    const mockRepository = createMockUserRepository();
+    const mockRepository = createMockGetAllRepository(mockUserList);
     const service = new UserService(mockRepository);
     const userList = await service.getAll();
-    expect(userList).toEqual(mockUserList);
+    const result = userList.value as User[];
+    const mock = mockUserList.value as User[];
+    expect(mock[0].name).toEqual(result[0].name); // テストの検証
   });
 });
