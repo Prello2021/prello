b6ca8f143452ed6a4caf595b5b0945eb25350bd8
hajime takeuchi <shadowthe8156@gmail.com>
2021-05-30 16:25:28 +0900

    テストのスケルトン作成

--- tests/repositories/UserRepository.test.ts
+++ tests/repositories/UserRepository.test.ts
@@ -1,15 +1,40 @@
+import { User } from "../../src/models/User";
+
 beforeEach(() => {
-  console.log("テスト前に走るよ!!!");
+  // Userテーブルの初期化
+  console.log("Userテーブルの初期化");
 });
 
 afterEach(() => {
-  console.log("テストが終わったよ!!!");
+  // Userテーブルの削除(データを空にする)
+  console.log("Userテーブルの削除(データを空にする)");
 });
 
-describe("sample", () => {
-  it("sample", () => {
-    console.log("テスト始めまーす!!");
-    const result = 1 + 3;
-    expect(result).toBe(4);
+describe("UserRepository 正常系テスト", () => {
+  it("getAll", () => {
+    // Userデータを3件insertする。
+    const createdUsers = createUserData(3);
+
+    // UserRepositoryをインスタンス化
+
+    // UserRepository.getAllを実行する
+
+    // 結果の県境
+
+    // 取得結果が3件であること
+
+    // 結果が createdUsers で返却されたデータと同じであること
+    expect(4).toBe(4);
   });
 });
+
+/**
+ * テスト用のUserデータを作成する
+ * @returns 作成されたUserデータ
+ */
+function createUserData(num: number): User[] {
+  // numの回数、サンプルのUserデータをInsertする。
+
+  // 作成したUserデータを返却する。
+  return [];
+}
