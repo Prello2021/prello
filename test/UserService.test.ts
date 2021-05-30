bc0d310f137976e13983701d4735e3d0bc3f0fb8
hajime takeuchi <shadowthe8156@gmail.com>
2021-05-30 15:05:43 +0900

    テスト途中まで

--- test/UserService.test.ts
+++ test/UserService.test.ts
@@ -3,27 +3,33 @@ import { UserService } from "../src/services/UserService";
 import { IUserRepository } from "../src/repositories/interfaces/IUserRepository";
 import { DatabaseResult } from "../src/utils/database/Database";
 
-const mockUserList: DatabaseResult<User[]> = {
-  value: [
-    {
-      id: 1,
-      name: "test1",
-      hobby: "test1",
-    },
-    {
-      id: 2,
-      name: "test2",
-      hobby: "test2",
-    },
-    {
-      id: 3,
-      name: "test3",
-      hobby: "test3",
-    },
-  ],
-  // error: null,
-};
+/**
+ * モックのユーザー情報(複数)を作成
+ *
+ * @param  {number} num
+ * @returns User
+ */
+function createMockUserData(num: number): User[] {
+  const userList: User[] = [];
 
+  for (let index = 0; index < num; index++) {
+    const user: User = {
+      id: index,
+      name: `name_${index}`,
+      hobby: `hobby_${index}`,
+    };
+    userList.push(user);
+  }
+
+  return userList;
+}
+
+/**
+ * GetAllUserRepositoryにモック化する
+ *
+ * @param  {DatabaseResult<User[]>} mockResult
+ * @returns IUserRepository
+ */
 function createMockGetAllRepository(
   mockResult: DatabaseResult<User[]>
 ): IUserRepository {
@@ -36,16 +42,63 @@ function createMockGetAllRepository(
 
   return mockRepository;
 }
-// ↑下準備
-// ↓テスト内容の実行
 
-describe("UserService 正常系テスト", () => {
+describe("UserService 正常系テスト(複数)", () => {
   it("getAll", async () => {
-    const mockRepository = createMockGetAllRepository(mockUserList);
+    // mockのユーザー情報結果を3件作成
+    const mockUserData = createMockUserData(3);
+    const mockResult: DatabaseResult<User[]> = {
+      value: mockUserData,
+    };
+
+    // Repositoryを作成し、Serviceをインスタンス化
+    const mockRepository = createMockGetAllRepository(mockResult);
     const service = new UserService(mockRepository);
+
+    // ユーザー取得実行
     const userList = await service.getAll();
     const result = userList.value as User[];
-    const mock = mockUserList.value as User[];
-    expect(mock[0].name).toEqual(result[0].name); // テストの検証
+
+    // 取得件数を検証する
+    expect(3).toBe(result.length);
+
+    // 取得結果の詳細を検証する
+    for (let index = 0; index < result.length; index++) {
+      expect(mockUserData[index].id).toBe(result[index].id);
+      expect(mockUserData[index].name).toBe(result[index].name);
+      expect(mockUserData[index].hobby).toBe(result[index].hobby);
+    }
+  });
+});
+
+describe("UserService 正常系テスト(空のとき)", () => {
+  it("getAll", async () => {
+    // mockのユーザー情報結果を0件作成
+    const mockResult: DatabaseResult<User[]> = {
+      value: [],
+    };
+
+    // Repositoryを作成し、Serviceをインスタンス化
+    const mockRepository = createMockGetAllRepository(mockResult);
+    const service = new UserService(mockRepository);
+
+    // ユーザー取得実行
+    const userList = await service.getAll();
+    const result = userList.value as User[];
+
+    // 取得件数を検証する
+    expect(0).toBe(result.length);
+    // エラーがないことを確認
+    expect(userList.error).toBeUndefined();
+  });
+});
+
+describe("UserService 異常系テスト(Repositoryでエラー)", () => {
+  it("getAll", async () => {
+    // mockのResultにエラーを入れておく
+    // Repositoryを作成し、Serviceをインスタンス化
+    // ユーザー取得実行
+    // エラーが入ってることを確認
+    // ステータスコードがRepositoryと同じであることを確認
   });
 });
